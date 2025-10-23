7f4638cdeff977e1915ae203fa399e59ab682823
Edsko de Vries <edsko@well-typed.com>
2025-10-23 11:00:48 +0200

    Leave a bit more space around exercise title

--- src/app/Exercises/Lilypond/Style.hs
+++ src/app/Exercises/Lilypond/Style.hs
@@ -24,14 +24,21 @@ data Class =
 isClass :: Ly.Markup.IsClass Class
 isClass = Ly.Markup.IsClass $ \case
     SectionTitle  -> Ly.Markup.Wrap sectionTitle
-    ExerciseTitle -> Ly.Markup.scope "bold"
+    ExerciseTitle -> Ly.Markup.Wrap exerciseTitle
 
 sectionTitle :: Doc -> Doc
 sectionTitle title =
     Ly.Markup.withinScope "center-column" $ mconcat [
         "\\vspace #1"
       , Ly.Markup.withinScope "abs-fontsize #16" $
-          Ly.Markup.withinScope "italic" $
-            title
+          Ly.Markup.withinScope "italic" title
       , "\\vspace #1"
+      ]
+
+exerciseTitle :: Doc -> Doc
+exerciseTitle title =
+    Ly.Markup.withinScope "center-column" $ mconcat [
+        "\\vspace #0.5"
+      , Ly.Markup.withinScope "bold" title
+      , "\\vspace #0.5"
       ]
