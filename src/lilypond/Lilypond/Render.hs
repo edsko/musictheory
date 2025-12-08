d41d2b575639fcb8ff0bc20d4795062962271919
Edsko de Vries <edsko@well-typed.com>
2025-12-08 14:16:44 +0100

    Bump revision number

--- src/lilypond/Lilypond/Render.hs
+++ src/lilypond/Lilypond/Render.hs
@@ -71,7 +71,7 @@ instance LilypondToDoc Ly.Book where
       , "\\markup \\vspace #2"
       , RenderM.markup $ Ly.Markup.italic $ mconcat [
             "Document produced by Edsko de Vries. "
-          , "Revision 4 (2025-11-25)"
+          , "Revision 5 (2025-12-08)"
           ]
       , "\\markup \\vspace #4"
       , "\\markuplist \\table-of-contents"
